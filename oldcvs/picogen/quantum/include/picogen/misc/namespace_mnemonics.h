5d8cab473b3de35ea165ad8b28a4857bd3d56de9
Sebastian Mach / phresnel <phresnel@gmail.com>
2008-05-23 16:20:05 +0000

    the picogen namespace can be unintuitive, so i will place some shorthand-namespaces here.
    git-svn-id: svn://svn.sv.gnu.org/picogen@120 31093add-a9ab-4058-a670-497b0c90ad29

---
+++ oldcvs/picogen/quantum/include/picogen/misc/namespace_mnemonics.h
@@ -0,0 +1,10 @@
+
+/***************************************************************************
+ *            namespace_mnemonics.h
+ *
+ *  Fr May 23 17:30:00 2008
+ *  Copyright  2008  Sebastian Mach
+ *  seb@greenhybrid.net
+ ****************************************************************************/
+
+namespace gob = picogen::graphics::objects;
