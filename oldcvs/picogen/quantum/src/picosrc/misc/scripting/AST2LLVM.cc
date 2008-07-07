0c3ca4894b24a64fc5c63a61b079b0e9743fde8c
Sebastian Mach / phresnel <phresnel@gmail.com>
2008-07-07 16:00:26 +0000

    * jit now running, yet unusable
    git-svn-id: svn://svn.sv.gnu.org/picogen@224 31093add-a9ab-4058-a670-497b0c90ad29

--- oldcvs/picogen/quantum/src/picosrc/misc/scripting/AST2LLVM.cc
+++ oldcvs/picogen/quantum/src/picosrc/misc/scripting/AST2LLVM.cc
@@ -28,17 +28,45 @@
 using namespace llvm;
 using namespace std;
 
-AST2LLVM::AST2LLVM() : verbose(true) {
+
+
+AST2LLVM::AST2LLVM()
+: builder(), module (new Module ("me jit")), executionEngine (0), entryPoint(0), verbose (true)
+{
 }
 
+
+
 AST2LLVM::~AST2LLVM () {
-    std::cout << endl;
+
     while (!values.empty()){
-        std::cout << values.top().value << endl;
         values.pop();
     }
-    std::cout << "-------------" << endl;
+
+    if (0 != executionEngine) {
+        delete executionEngine;
+        // From the comments in llvm's source code I read
+        //    /// create - This is the factory method for creating an execution engine which
+        //    /// is appropriate for the current machine.  This takes ownership of the
+        //    /// module.
+        // --> This means, if we have created an execution-engine, than we must no more
+        //     delete module ? or what?
+    } else {
+        if (0 != module) delete module;
+    }
 }
+
+
+
+void AST2LLVM::compile() {
+    module->dump();
+    executionEngine = ExecutionEngine::create (module);
+    entryPoint = executionEngine->getPointerToFunction (funtab [""]);
+    int (*FP)() = (int (*)())entryPoint;
+    fprintf(stderr, "Evaluated to %i\n", FP());
+}
+
+
 
 void AST2LLVM::visit (const FloatExprAST*) {}
 void AST2LLVM::visit (const IntExprAST*) {}
@@ -51,27 +79,75 @@ void AST2LLVM::visit (const BlockAST*) {}
 void AST2LLVM::visit (const IfBlockAST*) {}
 void AST2LLVM::visit (const WhileLoopAST*) {}
 void AST2LLVM::visit (const DoWhileLoopAST*) {}
-void AST2LLVM::visit (const FunProtoAST*) {}
-void AST2LLVM::visit (const FunAST*) {}
+
+
+
+void AST2LLVM::visit (const FunProtoAST* protoast) {
+    // Reminder:
+    //   FunctionType * get (const Type *Result, const std::vector< const Type * > &Params, bool isVarArg)
+
+    const Type *resultType = Type::Int32Ty;
+    std::vector< const Type * > paramTypes;
+
+    const std::vector<FunProtoAST::Argument> & args = protoast->getArguments();
+    for (std::vector<FunProtoAST::Argument>::const_iterator it = args.begin(); it != args.end(); ++it) {
+        switch (it->type) {
+            case int_type:
+                paramTypes.push_back (Type::Int32Ty);
+                break;
+            case float_type:
+                paramTypes.push_back (Type::FloatTy);
+                break;
+        };
+    }
+
+    FunctionType *funtype = FunctionType::get (resultType, paramTypes, false);
+    Function *fun = new Function (funtype, GlobalValue::ExternalLinkage, protoast->getName(), module);
+    Function::arg_iterator ai;
+    unsigned int srcidx = 0;
+    for (ai = fun->arg_begin(); ai != fun->arg_end(); ++ai, ++srcidx) {
+        ai->setName (args [srcidx].name);
+    }
+
+    // hmm
+    BasicBlock *BB = new BasicBlock ("entry", fun);
+    builder.SetInsertPoint(BB);
+
+    funtab [protoast->getName()] = fun;
+}
+
+
+
+void AST2LLVM::visit (const FunAST *ast) {
+}
 
 
 
 void AST2LLVM::end (const FloatExprAST* ast) {
     values.push (ValueDescriptor (llvm::ConstantFP::get(llvm::Type::FloatTy, llvm::APFloat (ast->getValue())), ast));
-    if (verbose) values.top().value->dump();
+    //if (verbose) values.top().value->dump();
 }
 
 
 
 void AST2LLVM::end (const IntExprAST* ast) {
     values.push (ValueDescriptor (llvm::ConstantInt::get(llvm::Type::Int32Ty, ast->getValue()), ast));
-    if (verbose) values.top().value->dump();
+    //if (verbose) values.top().value->dump();
 }
 
 
 
-void AST2LLVM::end (const IdExprAST*)  {
-
+void AST2LLVM::end (const IdExprAST *ast)  {
+    // A) Check if Id has been declared.
+    SymbolTable::const_iterator it = symtab.find (ast->getValue());
+    if (it == symtab.end()) {
+        // We have an undeclared variable.
+        /// \todo should this be handled by the frontend?
+        std::cout << "!!we have an undeclared var." << std::endl;
+        throw;
+    }
+    // B) Profit.
+    values.push (ValueDescriptor (symtab [ast->getValue()].llvmAlloca, ast));
 }
 
 
@@ -83,8 +159,25 @@ void AST2LLVM::end (const CallExprAST*)  {
 
 
 void AST2LLVM::end (const BinaryExprAST* ast)   {
-    Value *rhs = values.top().value;  values.pop();
-    Value *lhs = values.top().value;  values.pop();
+
+    // Load rhs.
+    Value *rhs   = values.top().value;
+    if (0 != dynamic_cast<const IdExprAST*> (values.top().ast)) {
+        // We got to load an id.
+        Value *valtmp = rhs;
+        rhs = builder.CreateLoad (valtmp);
+    }
+    values.pop();
+
+    // Load lhs.
+    Value *lhs   = values.top().value;
+    if (0 != dynamic_cast<const IdExprAST*> (values.top().ast)) {
+        // We got to load an id.
+        Value *valtmp = lhs;
+        lhs = builder.CreateLoad (valtmp);
+    }
+    values.pop();
+
 
     switch (ast->getOp()) {
         case '+':
@@ -100,31 +193,67 @@ void AST2LLVM::end (const BinaryExprAST* ast)   {
             values.push (ValueDescriptor (builder.CreateFDiv (lhs, rhs, "fdivtmp"), ast));
             break;
         default:
-            std::cerr << "ouch, unsupported operator in AST2LLVM: " << ast->getOp() << endl;
+            std::cerr << "!!ouch, unsupported operator in AST2LLVM: " << ast->getOp() << endl;
     };
-    if (verbose) values.top().value->dump();
+    //if (verbose) values.top().value->dump();
 }
 
 
 
 void AST2LLVM::end (const AssignmentExprAST *ast) {
-    /*Value *alloc   = values.top().value;  values.pop();
-    Value *initval = values.top().value;  values.pop();
-    //values.push (ValueDescriptor (builder.CreateStore (initval, alloc), ast));
-    if (verbose) values.top().value->dump();*/
+
+    // Load val.
+    Value *val   = values.top().value;
+    if (0 != dynamic_cast<const IdExprAST*> (values.top().ast)) {
+        // We got to load an id.
+        Value *valtmp = val;
+        val = builder.CreateLoad (valtmp);
+    }
+    values.pop();
+
+    // Next: to what we assign
+    Value *alloc = values.top().value;  values.pop();
+
+    // Finally create a store.
+    values.push (ValueDescriptor (builder.CreateStore (val, alloc), ast));
 }
 
 
 
-void AST2LLVM::end (const DeclarationAST*)  {
-    std::cout << "Declaration" << "-" << std::endl;
-    //ConstantFP::get(Type::DoubleTy, APFloat(Val));
+void AST2LLVM::end (const DeclarationAST *ast)  {
+    const Type *llvmType = 0;
+    switch (ast->getType()) {
+        case int_type:
+            llvmType = Type::Int32Ty;
+            break;
+        case float_type:
+            llvmType = Type::FloatTy;
+            break;
+    };
+
+    if (symtab.end() != symtab.find (ast->getName())) {
+        std::cout << "!! already declared!" << std::endl;
+        throw;
+    } else {
+        AllocaInst *tmp = builder.CreateAlloca (llvmType, 0, ast->getName().c_str());
+        symtab [ast->getName()] = Symbol (ast->getType(), ast->getName(), tmp);
+    }
 }
 
 
 
 void AST2LLVM::end (const BlockAST*)  {
-    //std::cout << "Block" << "-" << std::endl;
+    //std::cout << "Block - " << values.size() << std::endl;
+    // What we have now in values is what goes en blok.
+
+    //BasicBlock *BB = builder.Create ("entry",
+    /*
+    BasicBlock *BB = builder.GetInsertBlock ();
+    if (0 != BB) {
+        BasicBlock::iterator it;
+        for (it = BB->begin(); it != BB->end(); ++it) {
+        }
+    }*/
 }
 
 
@@ -147,12 +276,12 @@ void AST2LLVM::end (const DoWhileLoopAST*)  {
 
 
 
-void AST2LLVM::end (const FunProtoAST*)  {
-    //std::cout << "FunProto" << "-" << std::endl;
-}
+void AST2LLVM::end (const FunProtoAST *ast)  {}
 
 
 
-void AST2LLVM::end (const FunAST*)  {
-    //std::cout << "Fun" << "-" << std::endl;
+void AST2LLVM::end (const FunAST *ast) {
+    Value *retval = values.top().value; values.pop();
+    builder.CreateRet (retval);
+    verifyFunction (*funtab [ast->getPrototype()->getName()]);
 }
