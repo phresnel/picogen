28ba055c66a3c0d84910ca7f41ef2dc97286deaa
Sebastian Mach / phresnel <phresnel@gmail.com>
2008-08-17 13:55:53 +0000

    oh, forgot this file after the switch from samplers to integrators
    git-svn-id: svn://svn.sv.gnu.org/picogen@349 31093add-a9ab-4058-a670-497b0c90ad29

---
+++ oldcvs/picogen/quantum/include/picogen/graphics/integrators.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *            samplers.h
+ *
+ *  Thu Oct 11 19:58:36 2007
+ *  Copyright  2007  Sebastian Mach
+ *  root@greenhybrid.net
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 3 of the License, or (at your
+ *  option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+#ifndef _SAMPLERS_H
+#define _SAMPLERS_H
+
+#include "integrators/image/XYIterator.h"
+#include "integrators/ray/Simple.h"
+#include "integrators/ray/Preview.h"
+
+
+namespace picogen {
+    namespace graphics {
+        namespace integrators {
+            namespace screen {
+            }
+            namespace ray {
+            }
+            namespace light {
+            }
+        }
+    }
+}
+
+#endif /* _SAMPLERS_H */
