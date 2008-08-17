5eb33357b74486a62ac04d8188856d7fe6b7c473
Sebastian Mach / phresnel <phresnel@gmail.com>
2008-08-17 10:47:31 +0000

    primal scream
    git-svn-id: svn://svn.sv.gnu.org/picogen@331 31093add-a9ab-4058-a670-497b0c90ad29

---
+++ oldcvs/picogen/quantum/include/picogen/graphics/film.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+ *            film.h
+ *
+ *  2008-08-17 12:09:00 2008
+ *  Copyright  2007  Sebastian Mach
+ *  seb@greenhybrid.net
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 3 of the License, or (at your
+ *  option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef FILM_H__INCLUDED
+#define FILM_H__INCLUDED
+#endif // FILM_H__INCLUDED
