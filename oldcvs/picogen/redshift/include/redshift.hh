d8b2f25a264606476ad157cec8165ec4bd08d445
Sebastian Mach / phresnel <phresnel@gmail.com>
2009-02-25 18:17:59 +0000

    added some constants
    git-svn-id: svn://svn.sv.gnu.org/picogen@615 31093add-a9ab-4058-a670-497b0c90ad29

--- oldcvs/picogen/redshift/include/redshift.hh
+++ oldcvs/picogen/redshift/include/redshift.hh
@@ -40,15 +40,28 @@ namespace redshift {
         typedef CartesianNormal Normal;
         typedef CartesianVector Vector;
 
-        typedef kallisto::Sphere<Point,real_t>     Sphere;
-        typedef kallisto::Ray<Point,Vector>        Ray;
-        typedef kallisto::OrthonormalBasis<Vector> OrthonormalBasis;
+        typedef kallisto::Sphere<Point,long double>  Sphere;
+        typedef kallisto::Ray<Point,Vector>          Ray;
+        typedef kallisto::OrthonormalBasis<Vector>   OrthonormalBasis;
 
         struct IScene {};
 }
 
+namespace redshift {
+        namespace constants {
+                template<typename T> inline T km2m (T const &v) {
+                        return T(1000) * v;
+                }
+                static double const earth_radius_m     = km2m (12756.0);
+                static double const sun_radius_m       = km2m (1.392 * 10.0e9);
+                static double const jupiter_radius_m   = km2m (133708.0);
+                //static float const sun_radius_km 1,3914
+        }
+}
+
 
 // Rules: Every Color class must implement a method 'toRgb(real,real,real)'
+#include "contracts/color_contract.hh"
 #include "color/rgb.hh"
 #include "traits/color_traits.hh"
 
