c643c61fd07623e791452392a456dadffb47ffa5
Sebastian Mach / phresnel <phresnel@gmail.com>
2009-02-26 18:36:18 +0000

    primal scream
    git-svn-id: svn://svn.sv.gnu.org/picogen@616 31093add-a9ab-4058-a670-497b0c90ad29

---
+++ oldcvs/picogen/redshift/include/accelerators/bvhdef.hh
@@ -0,0 +1,28 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Copyright (C) 2009  Sebastian Mach (*1983)
+// * mail: phresnel/at/gmail/dot/com
+// * http://phresnel.org
+// * http://picogen.org
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#ifndef BVHDEF_H_INCLUDED_20090226
+#define BVHDEF_H_INCLUDED_20090226
+
+#include "bvh.hh"
+
+
+
+#endif // BVHDEF_H_INCLUDED_20090226
