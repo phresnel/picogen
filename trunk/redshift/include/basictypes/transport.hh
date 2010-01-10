dc0bf457f630c2a7e5b5b8646c3ae7c5d6fb0ca0
Sebastian Mach / phresnel <phresnel@gmail.com>
2010-01-10 18:11:54 +0100

    new interface redshift::Integrator

---
+++ trunk/redshift/include/basictypes/transport.hh
@@ -0,0 +1,41 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Copyright (C) 2009  Sebastian Mach (*1983)
+// * mail: phresnel/at/gmail/dot/com
+// * http://phresnel.org
+// * http://picogen.org
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#ifndef TRANSPORT_HH_INCLUDED_20090110
+#define TRANSPORT_HH_INCLUDED_20090110
+
+#include "../../include/setup.hh"
+#include "../../include/basictypes/differentialgeometry.hh"
+#include "../../include/basictypes/intersection.hh"
+#include "../../include/shapes/shape.hh"
+#include "../../include/shapes/closedsphere.hh"
+
+namespace redshift {
+        class Integrator {
+        public:
+                virtual tuple<real_t,Color> Li (
+                        const Scene &scene, 
+                        const RayDifferential &raydiff,
+                        const Sample &sample
+                ) const = 0;
+        };
+}
+
+#endif // TRANSPORT_HH_INCLUDED_20090110
