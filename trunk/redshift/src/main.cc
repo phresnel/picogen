990e8e51d1b106935378e33b7cb9846ad2562091
Sebastian Mach / phresnel <phresnel@gmail.com>
2010-02-22 07:21:59 +0100

    define basic structure of scene-files

--- trunk/redshift/src/main.cc
+++ trunk/redshift/src/main.cc
@@ -134,7 +134,200 @@ namespace {
 }
 
 
+
+namespace redshift { namespace scenefile {
+
+        // Object.
+        struct Object {
+                enum Type {
+                        horizon_plane,
+                        lazy_quadtree
+                };
+                static const actuarius::Enum<Type> Typenames;
+                Type type;
+
+                // Serialization.
+                template<typename Arch>
+                void serialize (Arch &arch) {
+                        using actuarius::pack;
+
+                }
+        };
+        const actuarius::Enum<Object::Type> Object::Typenames =
+                ( actuarius::Nvp<Object::Type>(Object::horizon_plane, "horizon-plane")
+                | actuarius::Nvp<Object::Type>(Object::lazy_quadtree, "lazy-quadtree")
+                );
+
+
+
+        // SurfaceIntegrator.
+        struct SurfaceIntegrator {
+                enum Type {
+                        redshift
+                };
+                static const actuarius::Enum<Type> Typenames;
+                Type type;
+
+                SurfaceIntegrator ()
+                : type(redshift)
+                {}
+
+                // Serialization.
+                template<typename Arch>
+                void serialize (Arch &arch) {
+                        using actuarius::pack;
+
+                        arch & pack("type", Typenames, type);
+                }
+        };
+        const actuarius::Enum<SurfaceIntegrator::Type> SurfaceIntegrator::Typenames =
+                ( actuarius::Nvp<SurfaceIntegrator::Type>(SurfaceIntegrator::redshift, "redshift")
+                );
+
+
+
+        // VolumeIntegrator.
+        struct VolumeIntegrator {
+                enum Type {
+                        none,
+                        emission,
+                        single
+                };
+                static const actuarius::Enum<Type> Typenames;
+                Type type;
+                float stepSize;
+
+                VolumeIntegrator ()
+                : type(none), stepSize(100.f)
+                {}
+
+                // Serialization.
+                template<typename Arch>
+                void serialize (Arch &arch) {
+                        using actuarius::pack;
+
+                        // TODO: check if this works bi-directionally
+                        if (Arch::serialize && type == none) {
+                                std::string bogo = "none";
+                                arch & pack (bogo);
+                        } else {
+                                arch & pack("type", Typenames, type);
+                                arch & pack("step-size", stepSize);
+                        }
+                }
+        };
+        const actuarius::Enum<VolumeIntegrator::Type> VolumeIntegrator::Typenames =
+                ( actuarius::Nvp<VolumeIntegrator::Type>(VolumeIntegrator::none, "none")
+                | actuarius::Nvp<VolumeIntegrator::Type>(VolumeIntegrator::emission, "emission")
+                | actuarius::Nvp<VolumeIntegrator::Type>(VolumeIntegrator::single, "single")
+                );
+
+
+
+        // RenderSettings.
+        struct RenderSettings {
+                unsigned int width, height;
+                std::string title;
+                SurfaceIntegrator surfaceIntegrator;
+                VolumeIntegrator volumeIntegrator;
+
+                // Serialization.
+                template<typename Arch>
+                void serialize (Arch &arch) {
+                        using actuarius::pack;
+
+                        //arch & pack ("title", title);
+                        arch & pack ("width", width);
+                        arch & pack ("height", height);
+                        arch & pack ("surface-integrator", surfaceIntegrator);
+                        arch & pack ("volume-integrator", volumeIntegrator);
+                }
+        };
+
+
+
+        // Scene = (RenderSettings+)(Objects*)
+        class Scene {
+                std::vector<Object> objects_;
+                std::vector<RenderSettings> renderSettings_;
+        public:
+                void addRenderSettings (RenderSettings const &rs) {
+                        renderSettings_.push_back (rs);
+                }
+                unsigned int renderSettingsCount() const {
+                        return renderSettings_.size();
+                }
+                RenderSettings const & renderSettings(unsigned int index) const {
+                        return renderSettings_[index];
+                }
+
+                void addObject (Object const &o) {
+                        objects_.push_back (o);
+                }
+                unsigned int objectCount() const {
+                        return objects_.size();
+                }
+                Object const & object(unsigned int index) const {
+                        return objects_[index];
+                }
+
+
+                // Serialization.
+                template<typename Arch>
+                void serialize (Arch &arch) {
+                        using actuarius::pack;
+
+                        arch & pack ("render-settings", &RenderSettings::title, renderSettings_);
+                        arch & pack ("objects", &Object::type, Object::Typenames, objects_);
+                }
+        };
+} }
+
+void actuarius_test () {
+        // TODO: make render settings an advice-thing, have multiple skies, have if-render-is member in sky (so that e.g. in "preview" there could be no ckouds)
+        using namespace redshift::scenefile;
+        using namespace actuarius;
+        Scene scene;
+
+        Object o;
+        o.type = Object::lazy_quadtree;
+        scene.addObject (o);
+        o.type = Object::horizon_plane;
+        scene.addObject (o);
+
+        RenderSettings rs;
+        rs.width = 800;
+        rs.height = 600;
+        rs.title = "preview";
+        scene.addRenderSettings (rs);
+        rs.width = 3200;
+        rs.height = 1600;
+        rs.title = "full";
+        scene.addRenderSettings (rs);
+
+        {std::ofstream ofs("test.red");
+        OArchive (ofs) & pack("scene", scene);}
+
+        {
+                // Evolve read function from this
+                Scene scene;
+                std::ifstream ifs("test.red");
+                IArchive (ifs) & pack("scene", scene);
+
+                if (scene.renderSettingsCount()>1) {
+                        std::cout << "There are multiple render settings present, \n";
+                        for (unsigned int i=0; i<scene.renderSettingsCount(); ++i) {
+                                std::cout << " [" << i << "] " << scene.renderSettings(i).title << "\n";
+                        }
+                        std::cout << ". Which one do you want to use? "<< std::endl;
+                }
+        }
+}
+
 int main (int argc, char *argv[]) {
+        actuarius_test();
+        return 0;
+
         const optional<Options> oo = parseOptions(argc,argv);
         if (!oo)
                 return 0;
