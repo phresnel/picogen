4feaba19cf892246d379f7818c79118fcbfb732f
Sebastian Mach / phresnel <phresnel@gmail.com>
2010-05-30 11:17:36 +0200

    move deb-related stuff to deb-folder

--- packagery/deb/make-version.sh
+++ packagery/deb/make-version.sh
@@ -2,13 +2,14 @@ VERSION=${1:-"<missing version>"}
 
 function make_arch {
         echo "putting ${1} to DEBS/${1}"
+        mkdir -p DEBS/${1}/
 
-        # create deb and control
-        cp -r deb.template DEBS/${1}
+        # copy deb and control
+        cp -r deb.template/* DEBS/${1}/
         ./make-control-file.sh ${VERSION} ${1} > DEBS/${1}/DEBIAN/control
 
         # copy changelog and ensure unix line endings
-        cp CHANGES.txt DEBS/${1}/DEBIAN/changelog
+        cp ../CHANGES.txt DEBS/${1}/DEBIAN/changelog
         fromdos DEBS/${1}/DEBIAN/changelog
 }
 
