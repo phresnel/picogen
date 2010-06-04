4ba544f65bc94049f6ee4d3355ba46ee10ba23c7
Sebastian Mach / phresnel <phresnel@gmail.com>
2010-06-04 18:17:23 +0200

    correct line endings

--- src/redshift/boost-libs.sh
+++ src/redshift/boost-libs.sh
@@ -1,9 +1,9 @@
 # a kludge to find boost libs properly and keep this away from the makefile.
 # and also to keep the msys installation minimal (/usr/lib points to /msys/1.0)
-if [ -e /usr/lib/libboost_program_options.a ] ; then 
+if [ -e /usr/lib/libboost_program_options.a ]; then
 	echo /usr/lib/libboost_program_options.a \
 		 /usr/lib/libboost_filesystem.a  \
-		 /usr/lib/libboost_system.a 
+		 /usr/lib/libboost_system.a
 else
 	echo /c/mingw/lib/libboost_program_options.a \
 		 /c/mingw/lib/libboost_filesystem.a \
