f084576020423a9a50337bb933bbe26d1d42ec21
Sebastian Mach / phresnel <phresnel@gmail.com>
2010-06-12 07:35:27 +0200

    also set permissions for binaries

--- packagery/deb/set-permissions-for-redist.sh
+++ packagery/deb/set-permissions-for-redist.sh
@@ -1,1 +1,3 @@
-chown -R root:root DEBS/* 2>/dev/null || echo "FAILED. Run me as root."
+$(chown -R root:root DEBS/* 2>/dev/null  \
+  && chmod a+x DEBS/*/usr/bin/*          \
+) || echo "FAILED. Run me as root."
