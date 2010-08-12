d37c39b2a11f5ebe5b605569e09052df05eadc6b
Sebastian Mach / phresnel <phresnel@gmail.com>
2010-08-12 20:15:45 +0200

    add skeleton for Bvh

---
+++ src/redshift/include/primitives/bvh.hh
@@ -0,0 +1,71 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Copyright (C) 2010  Sebastian Mach (*1983)
+// * mail: phresnel/at/gmail/dot/com
+// * http://phresnel.org
+// * http://picogen.org
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#ifndef Bvh_HH_INCLUDED_20100811
+#define Bvh_HH_INCLUDED_20100811
+
+#include "../sealed.hh"
+#include "accelerator.hh"
+
+namespace redshift { namespace primitive {
+
+        class BvhBuilder;
+
+        SEALED(Bvh);
+        class Bvh :
+                public Accelerator,
+                MAKE_SEALED(Bvh)
+        {
+        public:
+                ~Bvh ();
+
+                // -- BoundPrimitive ------------------------------------------
+                BoundingBox boundingBox() const;
+                // ------------------------------------------------------------
+
+                // -- Primitive -----------------------------------------------
+                bool doesIntersect (Ray const &) const;
+                optional<Intersection> intersect(Ray const &) const;
+                shared_ptr<Bsdf> getBsdf(const DifferentialGeometry &) const;
+                // ------------------------------------------------------------
+
+        private:
+                friend class BvhBuilder;
+                Bvh ();
+                Bvh& operator= (Bvh const &);
+                Bvh (Bvh const&);
+        };
+
+
+        SEALED(BvhBuilder);
+        class BvhBuilder :
+                public BoundPrimitive,
+                MAKE_SEALED(BvhBuilder)
+        {
+        public:
+                void add (Primitive const &prim);
+                shared_ptr<Bvh> toBvh() const;
+        private:
+                BvhBuilder (BvhBuilder const &);
+                BvhBuilder& operator= (BvhBuilder const&);
+        };
+} }
+
+#endif // ACCELERATOR_HH_INCLUDED_20100811
---
+++ src/redshift/src/primitives/bvh.cc
@@ -0,0 +1,78 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Copyright (C) 2010  Sebastian Mach (*1983)
+// * mail: phresnel/at/gmail/dot/com
+// * http://phresnel.org
+// * http://picogen.org
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#include "../../include/basictypes/intersection.hh"
+#include "../../include/primitives/bvh.hh"
+
+
+namespace redshift { namespace primitive {
+
+
+
+//-----------------------------------------------------------------------------
+// Bvh
+//-----------------------------------------------------------------------------
+Bvh::Bvh() {
+}
+
+
+
+Bvh::~Bvh() {
+}
+
+
+
+BoundingBox Bvh::boundingBox() const {
+        throw std::runtime_error("Bvh::boundingBox not implemented");
+}
+
+
+
+bool Bvh::doesIntersect (Ray const &) const {
+        throw std::runtime_error("Bvh::doesIntersect not implemented");
+}
+
+
+
+optional<Intersection> Bvh::intersect(Ray const &) const {
+        throw std::runtime_error("Bvh::intersect not implemented");
+}
+
+
+
+shared_ptr<Bsdf> Bvh::getBsdf(const DifferentialGeometry &) const {
+        throw std::runtime_error("Bvh::getBsdf not implemented");
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Builder
+//-----------------------------------------------------------------------------
+void BvhBuilder::add (Primitive const &prim) {
+}
+
+
+shared_ptr<Bvh> BvhBuilder::toBvh() const {
+        return shared_ptr<Bvh>();
+}
+
+
+} }
