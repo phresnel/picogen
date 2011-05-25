af063f856b44313e9db14a4423cb15d3a2bcf8ff
Sebastian Mach / phresnel <phresnel@gmail.com>
2011-05-26 00:13:34 +0200

    more file renames and more cmake.include's

---
+++ src/gui/CosyGui/cosyscene/include.cmake
@@ -0,0 +1,20 @@
+set (sources ${sources}
+        ${pwd}/camera.cc
+        ${pwd}/color.cc
+        ${pwd}/cosyfloat.cc
+        ${pwd}/direction3d.cc
+        ${pwd}/filmsettings.cc
+        ${pwd}/geometry.cc
+        ${pwd}/material.cc
+        ${pwd}/navigation.cc
+        ${pwd}/point3d.cc
+        ${pwd}/rendersettings.cc
+        ${pwd}/save_load.cc
+        ${pwd}/scene.cc
+        ${pwd}/sunsky.cc
+        ${pwd}/surfaceintegrator.cc
+        ${pwd}/terrain.cc
+        ${pwd}/vector3d.cc
+        ${pwd}/water.cc
+)
+include_directories ("${pwd}")
---
+++ src/renderer/redshift/include.cmake
@@ -0,0 +1,72 @@
+set (sources ${sources}
+        ${pwd}/src/constants.cc
+        ${pwd}/src/jobfile.cc
+        ${pwd}/src/quatsch-integration.cc
+        ${pwd}/src/get_compilation_info.cc
+        ${pwd}/src/save_openexr.cc
+
+        ${pwd}/src/backgrounds/pss-adapter.cc
+        ${pwd}/src/backgrounds/preetham-shirley-smits/atmconstants.cc
+        ${pwd}/src/backgrounds/preetham-shirley-smits/sunconstants.cc
+        ${pwd}/src/backgrounds/preetham-shirley-smits/sunsky.cc
+        ${pwd}/src/backgrounds/preetham-shirley-smits/tospectrum.cc
+
+        ${pwd}/src/basictypes/bsdf.cc
+        ${pwd}/src/basictypes/differentialgeometry.cc
+        ${pwd}/src/basictypes/intersection.cc
+        ${pwd}/src/basictypes/material.cc
+        ${pwd}/src/basictypes/scene.cc
+        ${pwd}/src/basictypes/spectrum.cc
+        ${pwd}/src/basictypes/volume.cc
+
+        ${pwd}/src/bxdf/brdftobtdf.cc
+        ${pwd}/src/bxdf/lambertian.cc
+        ${pwd}/src/bxdf/mirror.cc
+
+        ${pwd}/src/cameras/cubemapface.cc
+        ${pwd}/src/cameras/cylindrical.cc
+        ${pwd}/src/cameras/pinhole.cc
+
+        ${pwd}/src/integrators/emission.cc
+        ${pwd}/src/integrators/path.cc
+        ${pwd}/src/integrators/redshift.cc
+        ${pwd}/src/integrators/single-scattering.cc
+        ${pwd}/src/integrators/whitted.cc
+
+        ${pwd}/src/interaction/passivecommandprocessor.cc
+        ${pwd}/src/interaction/sdlcommandprocessor.cc
+
+        ${pwd}/src/material/leaf0.cc
+        ${pwd}/src/material/matte.cc
+
+        ${pwd}/src/primitives/boundinstance.cc
+        ${pwd}/src/primitives/bvh.cc
+        ${pwd}/src/primitives/closedsphere.cc
+        ${pwd}/src/primitives/forest.cc
+        ${pwd}/src/primitives/horizonplane.cc
+        ${pwd}/src/primitives/instance.cc
+        ${pwd}/src/primitives/lazyquadtree.cc
+        ${pwd}/src/primitives/list.cc
+        ${pwd}/src/primitives/lsystemtree.cc
+        ${pwd}/src/primitives/triangle.cc
+        ${pwd}/src/primitives/trianglebvh.cc
+        ${pwd}/src/primitives/waterplane.cc
+
+        ${pwd}/src/redshift_file/object.cc
+        ${pwd}/src/redshift_file/save_load.cc
+        ${pwd}/src/redshift_file/scene.cc
+        ${pwd}/src/redshift_file/to_redshift/object_to_redshift.cc
+        ${pwd}/src/redshift_file/to_redshift/redshift_file_to_redshift.cc
+
+        ${pwd}/src/rendertargets/colorrendertarget.cc
+        ${pwd}/src/rendertargets/sdlrendertarget.cc
+
+        ${pwd}/src/shapes/closedsphere.cc
+
+        ${pwd}/src/texture/image.cc
+
+        ${pwd}/src/volume/exponential.cc
+        ${pwd}/src/volume/homegeneous.cc
+        ${pwd}/src/volume/list.cc
+)
+include_directories ("${pwd}/include/")
