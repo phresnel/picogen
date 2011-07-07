9039c80496fba413fbfd1d50aac7035175835480
Sebastian Mach / phresnel <phresnel@gmail.com>
2011-07-07 10:47:10 +0200

    extract timing from picogen to aux

---
+++ src/auxiliary/glimpse/computationtime.hh
@@ -0,0 +1,60 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Copyright (C) 2010  Sebastian Mach (*1983)
+// * mail: phresnel/at/gmail/dot/com
+// * http://phresnel.org
+// * http://picogen.org
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#ifndef COMPUTATIONTIME_HH_INCLUDED_20110707
+#define COMPUTATIONTIME_HH_INCLUDED_20110707
+
+#include <ctime>
+namespace glimpse {
+
+	// Should give the total CPU time, i.e. if you have a quadcore box,
+	// then you get normal time * 4 (roughly, that is)
+        class ComputationTime {
+                double begin, end;
+                bool stopped;
+        public:
+                ComputationTime ()
+                : begin (clock() / (double)CLOCKS_PER_SEC)
+                , stopped(false)
+                {}
+
+                void restart () {
+                        stopped = false;
+                        begin = clock() / (double)CLOCKS_PER_SEC;
+                }
+
+                double stop () {
+                        stopped = true;
+                        end = clock() / (double)CLOCKS_PER_SEC;
+                        return end-begin;
+                }
+
+                double operator () () const volatile {
+                        if (stopped) {
+                                return end - begin;
+                        } else {
+                                return (clock() / (double)CLOCKS_PER_SEC) - begin;
+                        }
+                }
+
+        };
+}
+
+#endif // COMPUTATIONTIME_HH_INCLUDED_20110707
---
+++ src/auxiliary/glimpse/stopwatch.hh
@@ -0,0 +1,103 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Copyright (C) 2010  Sebastian Mach (*1983)
+// * mail: phresnel/at/gmail/dot/com
+// * http://phresnel.org
+// * http://picogen.org
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#ifndef STOPWATCH_HH_INCLUDED_20100707
+#define STOPWATCH_HH_INCLUDED_20100707
+
+#ifdef _OPENMP
+#include <omp.h>
+#include <iostream>
+namespace glimpse {
+        // Gives you the real world time, as opposed to ComputationTime.
+        class StopWatch {
+                double begin, end;
+                bool stopped;
+        public:
+                StopWatch ()
+                : begin (omp_get_wtime())
+                , stopped(false)
+                {}
+
+                void set (double time) {
+                        begin = (*this)()-time;
+                }
+
+                void restart () {
+                        *this = StopWatch();
+                }
+
+                double stop () {
+                        stopped = true;
+                        end = omp_get_wtime();
+                        return end-begin;
+                }
+
+                double operator () () const volatile {
+                        if (stopped) {
+                                return end - begin;
+                        } else {
+                                return omp_get_wtime() - begin;
+                        }
+                }
+
+        };
+}
+#else
+#pragma message "redshift:stopwatch.hh: falling back to clock(). Is this "\
+        "intentional? Otherwise, ensure that _OPENMP is defined. clock() is not"\
+        " what StopWatch intends to provide."
+#include <ctime>
+namespace glimpse {
+        class StopWatch {
+                double begin, end;
+                bool stopped;
+        public:
+                StopWatch ()
+                : begin (clock() / (double)CLOCKS_PER_SEC)
+                , stopped(false)
+                {}
+
+                void restart () {
+                        *this = StopWatch();
+                }
+
+                void set (double time) {
+                        begin = (*this)()-time;
+                }
+
+                double stop () {
+                        stopped = true;
+                        end = clock() / (double)CLOCKS_PER_SEC;
+                        return end-begin;
+                }
+
+                double operator () () const volatile {
+                        if (stopped) {
+                                return end - begin;
+                        } else {
+                                return (clock() / (double)CLOCKS_PER_SEC) - begin;
+                        }
+                }
+
+        };
+}
+#endif
+
+#endif // STOPWATCH_HH_INCLUDED_20100707
