6474bbd2c87c56771d84bb2bd5559bf20fae6911
Sebastian Mach / phresnel <phresnel@gmail.com>
2011-07-15 05:44:58 +0200

    add lerp, min, max for Point

--- src/picogen/cracker/point.h
+++ src/picogen/cracker/point.h
@@ -5,6 +5,7 @@
 #include "vector.h"
 
 #include <cassert>
+#include <algorithm>
 
 namespace picogen { namespace cracker {
 
@@ -17,6 +18,8 @@ public:
         real y() const { return y_; }
         real z() const { return z_; }
 
+        Point& operator += (Vector const &);
+
         real operator[] (unsigned int index) const {
                 assert (index<=2);
 
@@ -28,6 +31,29 @@ private:
 
         real x_, y_, z_;
 };
+
+
+inline Point lerp (Point const &lhs, Point const &rhs,
+                   real lerp_)
+{
+        return Point (lhs.x()*(1-lerp_) + rhs.x()*lerp_,
+                      lhs.y()*(1-lerp_) + rhs.y()*lerp_,
+                      lhs.z()*(1-lerp_) + rhs.z()*lerp_);
+}
+
+
+inline Point min (Point const &lhs, Point const &rhs) {
+        return Point (std::min(lhs.x(), rhs.x()),
+                      std::min(lhs.y(), rhs.y()),
+                      std::min(lhs.z(), rhs.z()));
+}
+
+
+inline Point max (Point const &lhs, Point const &rhs) {
+        return Point (std::max(lhs.x(), rhs.x()),
+                      std::max(lhs.y(), rhs.y()),
+                      std::max(lhs.z(), rhs.z()));
+}
 
 } }
 
