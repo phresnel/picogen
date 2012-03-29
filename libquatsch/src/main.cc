ed63d2da1b905c8d82e64b21faeec3355099676b
Sebastian Mach / phresnel <phresnel@gmail.com>
2012-03-29 10:19:06 +0200

    extract compile-functions from main()

--- libquatsch/src/main.cc
+++ libquatsch/src/main.cc
@@ -28,15 +28,6 @@ private:
 Instantiation
 Test::instantiate_ (std::list<StaticParameter> const &params) const
 {
-        for (auto const &p : params) {
-                std::cout << p.name() << " : ";
-                switch (p.type()) {
-                case Integer: std::cout << "int <- " << p.integer(); break;
-                case Float:   std::cout << "float <- " << p.floating(); break;
-                case String:  std::cout << "string <- " << p.string(); break;
-                }
-                std::cout << '\n';
-        }
         return { [](DynamicArguments const &args){
                         return DynamicVariant::Floating(args[0].floating()*
                                                         args[1].floating());
@@ -50,49 +41,61 @@ Test::instantiate_ (std::list<StaticParameter> const &params) const
 } }
 
 
-int main () {
+quatsch::compiler::phase3::ProgramPtr
+compile_phase3_program (std::string const &code,
+                        std::list<quatsch::extern_template::TemplatePtr> const & templates
+                        )
+{
         using namespace quatsch::extern_template;
-        std::cout << '\n';
-
         using namespace quatsch::compiler;
-        const std::string code =
-        "\n"
-        "(let pi:float 3.14159)\n"
-        "(let two:float 3.14159)\n"
-        "(defun main:int (x:int y) (main 2i 3))\n" // TODO: shall we? would have re-nameable program input
-        //"([Test frob{3} foo{1 2 3}] 2 2)"
-        "(defun T (x:int) (/ y y))\n"
-        "([Test frob{1.5}] x y)"
 
-        ;
-        std::cout << "quatsch code:\n------------\n" << code
-                  << "\n------------\n";
-
-        std::cout << "-- phase 1 -----" << std::endl;
         phase1::Toque toks = phase1::tokenize (code.begin(), code.end());
-
-        std::cout << "-- phase 2 -----" << std::endl;
         phase2::ProgramPtr P2 = phase2::parse(toks);
         if (!P2) {
                 std::cerr << "parse error" << std::endl;
                 return 0;
         }
 
-        std::cout << "-- phase 3 -----" << std::endl;
-        std::list<quatsch::extern_template::TemplatePtr> templates;
-        templates.emplace_back (new Test());
         phase3::ProgramPtr P3 = phase3::resolve_and_verify (*P2, templates);
         if (!P3) {
                 std::cerr << "verficiation/resolution error" << std::endl;
                 return 0;
         }
-        std::string c99 = phase5::to_C99 (*P3);
-        std::cout << "C99 code:\n------------\n" << c99
-                  << "\n------------\n";
+        return P3;
+}
 
-        auto fun = phase5::to_callable (*P3);
-        DynamicArguments args ({DynamicVariant::Floating(0.5),
-                                DynamicVariant::Floating(0.5)});
+
+std::function<quatsch::extern_template::DynamicVariant(
+                quatsch::extern_template::DynamicArguments const&
+              )>
+compile_callable (std::string const &code,
+                  std::list<quatsch::extern_template::TemplatePtr> const & templates)
+{
+        using namespace quatsch::compiler;
+        phase3::ProgramPtr P3 = compile_phase3_program (code, templates);
+        return phase5::to_callable (*P3);
+}
+
+
+int main () {
+        using namespace quatsch;
+        namespace qe = quatsch::extern_template;
+
+        std::list<qe::TemplatePtr> templates;
+        templates.emplace_back (new qe::Test());
+        auto fun = compile_callable (
+                "\n"
+                "(let pi:float 3.14159)\n"
+                "(let two:float 3.14159)\n"
+                "(defun main:int (x:int y) (main 2i 3))\n"
+                //"([Test frob{3} foo{1 2 3}] 2 2)"
+                "(defun T (x:int) (/ y y))\n"
+                "([Test frob{1.5}] x y)",
+                templates
+        );
+
+        qe::DynamicArguments args ({qe::DynamicVariant::Floating(0.5),
+                                    qe::DynamicVariant::Floating(0.5)});
         std::cout << fun (args).floating() << std::endl;
 }
 
