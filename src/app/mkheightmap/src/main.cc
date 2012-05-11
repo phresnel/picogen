e017751461b51841e7ae9c0ed648c095b5fc7395
Sebastian Mach / phresnel <phresnel@gmail.com>
2012-05-11 09:14:00 +0200

    create ppm from simple (hardcoded) quatsch-function

--- src/app/mkheightmap/src/main.cc
+++ src/app/mkheightmap/src/main.cc
@@ -1,34 +1,44 @@
 #include "quatsch.h"
 #include "phase5/callable/to_callable.h"
+#include "ppm.h"
 
 int main ()
 {
         using namespace quatsch;
 
         try {
-                std::string code = "(defun foo (x y) (* x y))\n"
-                                   "(foo 1 2)";
+                std::string     code = "(defun foo (x y) (* x y))\n"
+                                       "(* x z)";
                 TemplatePtrList templates;
-                ErrorState errors;
+                ErrorState      errors;
                 ProgramContract ptype (Typename::Float,
                                   {{"x",Typename::Float},
                                    {"z",Typename::Float}});
 
                 quatsch::quatsch_function fun = quatsch::compile (
-                                code, templates, ptype, errors, compiler::phase5::to_callable);
+                                code, templates, ptype, errors,
+                                compiler::phase5::to_callable);
 
                 if (errors) {
                         print_errors (errors, std::cerr);
                         return EXIT_FAILURE;
                 }
 
-                DynamicArguments args;
-                args.push_back (DynamicVariant::Integer (2));
-                args.push_back (DynamicVariant::Floating (3));
-                //$ TODO: expected error
-                const DynamicVariant ret = fun (args);
+                const int width = 240, height = 240;
 
-                std::cout << ret.integer() << std::endl; //$ TODO: should be no error
+                image::write_ppm (std::cout, width, height, [=](int x, int y) {
+                        // Transform [0..image-size) -> [-0.5..0.5)
+                        const double u_ = (x / static_cast<double> (width))-0.5,
+                                     v_ = ((height-1-y) / static_cast<double> (height))-0.5;
+
+                        // Call height-function.
+                        const auto   h_ = fun ({DynamicVariant::Floating(u_),
+                                                DynamicVariant::Floating(v_)});
+
+                        // Done!
+                        const double h = h_.floating();
+                        return std::make_tuple(h, h, h);
+                });
 
         } catch (std::exception &e) {
                 std::cerr << "error: " << e.what() << '\n';
