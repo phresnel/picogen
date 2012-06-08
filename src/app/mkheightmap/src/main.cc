8651cdbcaf39112973dee2a3ffb45a4c618f60c1
Sebastian Mach / phresnel <phresnel@gmail.com>
2012-06-08 09:33:07 +0200

    refactor for maintainability

--- src/app/mkheightmap/src/main.cc
+++ src/app/mkheightmap/src/main.cc
@@ -4,48 +4,55 @@
 #include "argxx.h"
 #include <fstream>
 
+struct ProgramOptions {
+        int width, height;
+        std::string code;
+};
+
+ProgramOptions parse_options (int argc, char *argv[])
+{
+        auto args = argxx::parse (argc, argv);
+        const auto code_opt     = argxx::optional<std::string> (args, {'c', "code"});
+        const auto filename_opt = argxx::optional<std::string> (args, {'f', "filename"});
+        if ((!code_opt && !filename_opt) || (code_opt && filename_opt)) {
+                throw std::runtime_error("error: either pass --code=<code>, "
+                                         "or --filename=<quatsch-file>");
+        }
+        const auto width  = argxx::optional_with_default<unsigned> (args, {'w', "width"}, 256);
+        const auto height = argxx::optional_with_default<unsigned> (args, {'h', "height"}, width);
+        argxx::assert_no_unparsed_present (args);
+
+        std::string code;
+        if (filename_opt) {
+                std::ifstream ifs (*filename_opt);
+                if (!ifs.good())
+                        throw std::runtime_error("error: could not open file \""
+                                                 + *filename_opt + "\"");
+
+                std::string line;
+                while (getline (ifs, line))
+                        code += line;
+        } else {
+                code = *code_opt;
+        }
+
+        return {width, height, code};
+}
+
 int main (int argc, char *argv[])
 {
         using namespace quatsch;
 
         try {
-                auto args = argxx::parse (argc, argv);
-                const auto code_opt     = argxx::optional<std::string> (args, {'c', "code"});
-                const auto filename_opt = argxx::optional<std::string> (args, {'f', "filename"});
-                if ((!code_opt && !filename_opt) || (code_opt && filename_opt)) {
-                        throw std::runtime_error("error: either pass --code=<code>, "
-                                                 "or --filename=<quatsch-file>");
-                }
-                const auto width  = argxx::optional_with_default<unsigned> (args, {'w', "width"}, 256);
-                const auto height = argxx::optional_with_default<unsigned> (args, {'h', "height"}, width);
-                argxx::assert_no_unparsed_present (args);
+                const ProgramOptions opts = parse_options (argc, argv);
 
-                // TODO: extract function
-                std::string code;
-                if (filename_opt) {
-                        std::ifstream ifs (*filename_opt);
-                        if (!ifs.good())
-                                throw std::runtime_error("error: could not open file \""
-                                                         + *filename_opt + "\"");
+                const TemplatePtrList templates;
+                const ProgramContract ptype (Typename::Float, {{"x", Typename::Float},
+                                                               {"z", Typename::Float}});
 
-                        std::string line;
-                        while (getline (ifs, line))
-                                code += line;
-                } else {
-                        code = *code_opt;
-                }
-                /*
-                std::string     code = "(defun foo (x y) (* x y))\n"
-                                       "(* x z)";
-                */
-                TemplatePtrList templates;
-                ErrorState      errors;
-                ProgramContract ptype (Typename::Float,
-                                  {{"x",Typename::Float},
-                                   {"z",Typename::Float}});
-
+                ErrorState errors;
                 quatsch::quatsch_function fun = quatsch::compile (
-                                code, templates, ptype, errors,
+                                opts.code, templates, ptype, errors,
                                 compiler::phase5::to_callable);
 
                 if (errors) {
@@ -53,10 +60,13 @@ int main (int argc, char *argv[])
                         return EXIT_FAILURE;
                 }
 
-                image::write_ppm (std::cout, width, height, [=](int x, int y) {
+                const auto iwidth  = 1. / static_cast<double> (opts.width),
+                           iheight = 1. / static_cast<double> (opts.height);
+
+                image::write_ppm (std::cout, opts.width, opts.height, [=](int x, int y) {
                         // Transform [0..image-size) -> [-0.5..0.5)
-                        const double u_ = (x / static_cast<double> (width))-0.5,
-                                     v_ = ((height-1-y) / static_cast<double> (height))-0.5;
+                        const double u_ = -0.5 + iwidth * x,
+                                     v_ = -0.5 + iheight * (opts.height-1-y);
 
                         // Call height-function.
                         const auto   h_ = fun ({DynamicVariant::Floating(u_),
